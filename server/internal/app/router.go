becb7fd49ccee232fbcb78bbf20611db3f393366
yami <ravirrajpatil@gmail.com>
2026-03-23 23:56:41 +0530

    Implement WebSocket routing and health check endpoint

--- server/internal/app/router.go
+++ server/internal/app/router.go
@@ -1,4 +1,20 @@
 package app
 
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/ravirraj/inkRush/server/internal/transport/websoc/handler"
+)
 
+func NewRouter(wsHandler *handler.WebSocketHandler) *gin.Engine {
+	router := gin.Default()
+	router.GET("/ws", wsHandler.Handle)
+	router.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.
+			H{
+			"status": "Health Ok",
+		})
+	})
+	return router
+}
